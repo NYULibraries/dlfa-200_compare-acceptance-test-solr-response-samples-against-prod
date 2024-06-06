e4b7e9c8201539290c80e1c0f915e8b8a795deac
David <da70@nyu.edu>
2024-06-06 17:45:40 -0400

    diff-samples.sh: use long option names

--- diff-samples.sh
+++ diff-samples.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-diff -X exclude-list.txt -r prod/ local/ | gegrep -E -v -- '---|        "_version_":|         "timestamp":"|^[0-9,c]+$|    "QTime":|^diff -r |^<         "Translation missing: en.ead_indexer.fields.dao",|^>         "Online Access",'
+diff --exclude-from=exclude-list.txt --recursive prod/ local/ | gegrep --extended-regexp --invert-match -- '---|        "_version_":|         "timestamp":"|^[0-9,c]+$|    "QTime":|^diff -r |^<         "Translation missing: en.ead_indexer.fields.dao",|^>         "Online Access",'
 
