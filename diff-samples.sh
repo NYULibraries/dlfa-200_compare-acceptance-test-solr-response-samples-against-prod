b952f00ca4e2c9d45867eefbb2b2959fc758ec0b
David <da70@nyu.edu>
2024-06-07 11:29:43 -0400

    diff-samples.sh: write out individual diff files and run with fixed exclude-list

--- diff-samples.sh
+++ diff-samples.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-diff --exclude-from=exclude-list.txt --recursive prod/ local/ | gegrep --extended-regexp --invert-match -- '---|        "_version_":|         "timestamp":"|^[0-9,c]+$|    "QTime":|^diff -r |^<         "Translation missing: en.ead_indexer.fields.dao",|^>         "Online Access",'
+ROOT=$( cd "$(dirname "$0")" ; pwd -P )
 
+LOCAL=$ROOT/local
+PROD=$ROOT/prod
+DIFFS=$ROOT/diffs
+
+rm -fr $DIFFS
+
+for prod_file in $( find $PROD -type f | grep -v -f $ROOT/exclude-list.txt | sort ); do
+    qualified_filename=$( echo $prod_file | sed "s@$PROD/@@" )
+    local_file=$LOCAL/$qualified_filename
+    diff_output=$( diff $prod_file $local_file | gegrep --extended-regexp --invert-match -- '---|        "_version_":|         "timestamp":"|^[0-9,c]+$|    "QTime":|^diff -r |^<         "Translation missing: en.ead_indexer.fields.dao",|^>         "Online Access",' )
+    if [ ! -z "$diff_output" ]; then
+        diff_file=$DIFFS/$qualified_filename
+        echo "Writing diff file $diff_file"
+
+        diff_dir=$( dirname $diff_file )
+        mkdir -p $diff_dir
+
+        echo "$diff_output" > $diff_file
+    fi
+done
